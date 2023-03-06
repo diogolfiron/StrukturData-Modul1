21c12174aa4939ae05e42a5e7b92690dc4fbd8d0
diogolfiron <102617775+diogolfiron@users.noreply.github.com>
2023-03-06 20:23:54 +0700

    Add files via upload

---
+++ modul1/src/modul1/Makanan.java
@@ -0,0 +1,6 @@
+package modul1;
+public class Makanan extends Hidangan {
+    public String disantap() {
+        return this.getNamaHidangan() + " dimakan";
+    }
+}
---
+++ modul1/src/modul1/Minuman.java
@@ -0,0 +1,6 @@
+package modul1;
+public class Minuman extends Hidangan{
+    public String disantap() {
+        return this.getNamaHidangan() + " diminum";
+    }
+}
